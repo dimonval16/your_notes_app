26d2da07b69ee124032630756082011a837b803c
Dmytro Volynskyi <dimonval16@gmail.com>
2020-02-25 19:47:42 +0200

    refactoring

--- react_task_manager/src/components/Main_content/Notes/Notes.jsx
+++ react_task_manager/src/components/Main_content/Notes/Notes.jsx
@@ -4,11 +4,12 @@ import NotesStyle from '../Main_content.module.css';
 import Note from './Note/Note';
 
 const Notes = (props) => {
+
+    const notes = props.notes.map(el => <Note title={el.title} completed={el.completed} key={el.id}/>);
+
     return(
         <div className={NotesStyle.items}>
-            {props.notes.map(el => 
-                <Note title={el.title} completed={el.completed} key={el.id}/>)
-            }
+            { notes }
         </div>
     );
 }
--- react_task_manager/src/components/Sidebar/Category/Category.jsx
+++ react_task_manager/src/components/Sidebar/Category/Category.jsx
@@ -3,11 +3,12 @@ import CategoryItem from './CategoryItem/CategoryItem';
 import CategoryBlock from './Category.module.css'
 
 const Category = (props) => {
+
+    const categoryItems = props.categories.map(el => <CategoryItem title={el.title} key={el.id}/>);
+
     return(
         <div className={CategoryBlock.category_block}>
-            {props.categories.map(el => 
-                <CategoryItem title={el.title} key={el.id}/>
-            )}
+            { categoryItems }
         </div>
     );
 }
